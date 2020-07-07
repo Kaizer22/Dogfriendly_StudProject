ad9d0abd4463d3a0f3cc33d4c6a1181d340c7350
posahok <prostak.sasha111@mail.ru>
2020-07-07 20:19:35 +0300

    Creating google map

--- presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/ui/activity/MainActivity.kt
+++ presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/ui/activity/MainActivity.kt
@@ -3,10 +3,33 @@ package com.lanit_tercom.dogfriendly_studproject.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lanit_tercom.dogfriendly_studproject.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.lanit_tercom.dogfriendly_studproject.ui.fragment.MapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        googleMap?.apply {
+            val sydney = LatLng(-33.852, 151.211)
+            addMarker(
+                    MarkerOptions()
+                            .position(sydney)
+                            .title("Marker in Sydney")
+            )
+            // [START_EXCLUDE silent]
+            moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            // [END_EXCLUDE]
+        }
     }
 }
