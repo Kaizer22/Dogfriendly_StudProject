706897326083b6f1c3af2e0350590bdce2389ef0
Perevalov Pavel <perevalovpaul@gmail.com>
2020-07-13 19:52:22 +0300

    Подключена зависимость, создан тестовый класс в соответствующем пакете

---
+++ domain/src/test/java/com/lanit_tercom/domain/interactor/get/GetUserDetailsTest.java
@@ -0,0 +1,21 @@
+package com.lanit_tercom.domain.interactor.get;
+
+import com.lanit_tercom.domain.repository.UserRepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GetUserDetailsTest {
+    private GetUserDetailsUseCase useCase;
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() {
+        //userRepository = Mockito.mock(UserRepository.class);
+    }
+
+    @Test
+    void testRunMethodWithGetUser() {
+
+    }
+}
