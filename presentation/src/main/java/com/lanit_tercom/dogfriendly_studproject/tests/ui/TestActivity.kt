34b51559955be805a1a7c2bdb5eed45fd65e1fc8
Nikolay <nikolaygorokhov1@gmail.com>
2020-07-29 23:10:55 +0300

    Тут длиннотекст, поэтому на русиче. Я вроде как понял работать с CoordinatorLayout где скрываемый элемент находится сверху, но совсем не понимаю как поставить его ниже других элементов (в данном случае - аватара, и полей имени и возраста) и как это вообще будет выглядеть после этого. Может для этого в разметке можно использовать какой-то другой, более оптимальный подход? Нерабочий вариант с CoordinatorLayout находится в user_detail_test.xml. Так же есть еще один вариант - user_detail_test2.xml. В нем реализован примерно похожий функционал, но без слайда - через нажатие кнопки (она там сейчас не очень удачно расположена, но можно перетащить потом). Большой недостаток тут в том, что оно работает не только через разметку в макете, но и через код в активити (я взял это отсюда: https://www.youtube.com/watch?v=19E85PngufY). Но зато работает хоть как-то.

--- presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
+++ presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
@@ -1,53 +1,47 @@
 package com.lanit_tercom.dogfriendly_studproject.tests.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import com.lanit_tercom.dogfriendly_studproject.R
-import com.lanit_tercom.dogfriendly_studproject.data.auth_manager.AuthManager
-import com.lanit_tercom.dogfriendly_studproject.data.entity.ChannelEntity
-import com.lanit_tercom.dogfriendly_studproject.data.entity.UserEntity
-import com.lanit_tercom.dogfriendly_studproject.data.executor.JobExecutor
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.channel.ChannelEntityStore
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.channel.ChannelEntityStoreFactory
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.channel.FirebaseChannelEntityStore
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.user.UserEntityStoreFactory
-import com.lanit_tercom.dogfriendly_studproject.data.mapper.ChannelEntityDtoMapper
-import com.lanit_tercom.dogfriendly_studproject.data.mapper.UserEntityDtoMapper
-import com.lanit_tercom.dogfriendly_studproject.data.repository.ChannelRepositoryImpl
-import com.lanit_tercom.dogfriendly_studproject.data.repository.UserRepositoryImpl
-import com.lanit_tercom.dogfriendly_studproject.executor.UIThread
-import com.lanit_tercom.domain.dto.ChannelDto
-import com.lanit_tercom.domain.dto.UserDto
-import com.lanit_tercom.domain.exception.ErrorBundle
-import com.lanit_tercom.domain.executor.PostExecutionThread
-import com.lanit_tercom.domain.executor.ThreadExecutor
-import com.lanit_tercom.domain.interactor.channel.AddChannelUseCase
-import com.lanit_tercom.domain.interactor.channel.GetChannelsUseCase
-import com.lanit_tercom.domain.interactor.channel.impl.AddChannelUseCaseImpl
-import com.lanit_tercom.domain.interactor.channel.impl.GetChannelsUseCaseImpl
-import com.lanit_tercom.domain.interactor.user.CreateUserDetailsUseCase
-import com.lanit_tercom.domain.interactor.user.EditUserDetailsUseCase
-import com.lanit_tercom.domain.interactor.user.GetUserDetailsUseCase
-import com.lanit_tercom.domain.interactor.user.GetUsersDetailsUseCase
-import com.lanit_tercom.domain.interactor.user.impl.CreateUserDetailsUseCaseImpl
-import com.lanit_tercom.domain.interactor.user.impl.EditUserDetailsUseCaseImpl
-import com.lanit_tercom.domain.interactor.user.impl.GetUserDetailsUseCaseImpl
-import com.lanit_tercom.domain.interactor.user.impl.GetUsersDetailsUseCaseImpl
-import com.lanit_tercom.domain.repository.UserRepository
-import com.lanit_tercom.library.data.manager.NetworkManager
-import com.lanit_tercom.library.data.manager.impl.NetworkManagerImpl
-import kotlinx.android.synthetic.main.activity_test.*
-import java.util.*
 
 
+/**
+ * Закоменченный код - нерабочая версия с CollapsingToolBar
+ */
 class TestActivity : AppCompatActivity() {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.user_detail_test)
+//    }
+
+    var expandableView: LinearLayout? = null
+    var openDescButton: ImageButton? = null
+    var cardView: CardView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.user_detail_test)
+        setContentView(R.layout.user_detail_test2)
+        expandableView = findViewById(R.id.expandable_desc)
+        cardView = findViewById(R.id.cardView)
+        openDescButton = findViewById(R.id.open_desc_button)
 
+        openDescButton?.setOnClickListener(View.OnClickListener {
+            if (expandableView?.visibility == View.GONE) {
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                expandableView?.visibility = View.VISIBLE
+
+            } else {
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition())
+                expandableView?.visibility = View.GONE
+            }
+        })
     }
-
 }
