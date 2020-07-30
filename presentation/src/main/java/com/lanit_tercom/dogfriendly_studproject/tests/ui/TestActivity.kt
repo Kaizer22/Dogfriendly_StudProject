fc7688a46456e6b64f19210216069fa24273cb46
posahok <prostak.sasha111@mail.ru>
2020-07-31 00:16:38 +0300

    user_map xml (added seekbar, bottom sheet, etc...)

--- presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
+++ presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
@@ -1,64 +1,54 @@
 package com.lanit_tercom.dogfriendly_studproject.tests.ui
 
-import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+
 import com.lanit_tercom.dogfriendly_studproject.R
-import com.lanit_tercom.dogfriendly_studproject.data.auth_manager.firebase_impl.AuthManagerFirebaseImpl
-import com.lanit_tercom.dogfriendly_studproject.data.entity.MessageEntity
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.message.FirebaseMessageEntityStore
-import com.lanit_tercom.dogfriendly_studproject.data.firebase.message.MessageEntityStore
-import com.lanit_tercom.dogfriendly_studproject.data.geofire.UserGeoFire
-import com.lanit_tercom.dogfriendly_studproject.ui.activity.UserSignInActivity
-import com.lanit_tercom.domain.exception.ErrorBundle
-import kotlinx.android.synthetic.main.activity_test.*
-import java.lang.Exception
-import java.sql.Timestamp
+import kotlinx.android.synthetic.main.user_map_test.*
 
+class TestActivity : AppCompatActivity() {
 
-class TestActivity : AppCompatActivity(), View.OnClickListener {
+    private var googleMap: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_map_test)
-//        button_post.setOnClickListener(this)
-//        button_get.setOnClickListener(this)
+        button_radar.setOnClickListener {
 
-    }
+            if (search_container.visibility == View.VISIBLE) return@setOnClickListener
 
-    override fun onClick(p0: View?) {
+            seekbar_container.visibility = View.VISIBLE
+            textView_radar.visibility = View.VISIBLE
+            seekBar.visibility = View.VISIBLE
 
-        when (p0?.id) {
-            R.id.button_post -> {
-                val userName = editText_userName.text.toString()
-                val body = editText_body.text.toString()
-                val channelId = editText_channelId.text.toString()
-                val message = MessageEntity(userName, body)
-                message.channelId = channelId
-                FirebaseMessageEntityStore(null).postMessage(message, object: MessageEntityStore.MessagePostCallback{
-                    override fun onError(errorBundle: ErrorBundle?) {
-                        Toast.makeText(this@TestActivity, errorBundle?.errorMessage, Toast.LENGTH_LONG).show()
-                    }
+            seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
 
-                    override fun onMessagePosted() {
-                        Toast.makeText(this@TestActivity, "Posted!", Toast.LENGTH_LONG).show()
-                    }
-                })
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    seekBar?.visibility = View.GONE
+                    seekbar_container.visibility = View.GONE
+                    textView_radar.visibility = View.GONE
+                }
+            })
+        }
+        button_location.setOnClickListener {
+            if (seekbar_container.visibility == View.VISIBLE) return@setOnClickListener
+            search_container.visibility = View.VISIBLE
+            button_search.visibility = View.VISIBLE
+
+            button_search.setOnClickListener {
+                search_container.visibility = View.GONE
+                button_search.visibility = View.GONE
             }
-            R.id.button_get -> {
-                FirebaseMessageEntityStore(null).getMessages(editText_channelId.text.toString(), object: MessageEntityStore.MessagesDetailCallback{
-                    override fun onError(errorBundle: ErrorBundle?) {
-                        textView.text = errorBundle?.errorMessage
-                    }
-
-                    override fun onMessagesLoaded(messages: MutableList<MessageEntity>?) {
-                        messages?.forEach {textView.append("timestamp: ${it.timestampForMapper}\n")}
-                    }
-                })
-            }
-
         }
     }
 
