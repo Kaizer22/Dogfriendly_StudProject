328012ebee9df584f7e459bc9c10b8c990552485
Женя Викулов <jenyav94@gmail.com>
2020-07-31 18:56:52 +0300

    appbar dynamic height update

--- presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
+++ presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/tests/ui/TestActivity.kt
@@ -1,14 +1,10 @@
 package com.lanit_tercom.dogfriendly_studproject.tests.ui
 
 import android.os.Bundle
-import android.transition.AutoTransition
-import android.transition.TransitionManager
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
 import com.lanit_tercom.dogfriendly_studproject.R
 
 
@@ -20,6 +16,11 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_detail_test)
+
+        val appbar = findViewById<View>(R.id.appbar) as AppBarLayout
+        val heightDp = resources.displayMetrics.heightPixels  * 0.5 - (48+10) * resources.displayMetrics.density
+        val lp = appbar.layoutParams as CoordinatorLayout.LayoutParams
+        lp.height = heightDp.toInt()
     }
 
 }
