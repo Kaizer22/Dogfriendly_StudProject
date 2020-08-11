c215fd05c4e70143e4cb21bc665ce64c1fec8257
Antropov Daniil <adv980730@mail.ru>
2020-08-11 22:57:35 +0300

    Создал макет для экрана прогулки, пока без привязки данных.

---
+++ presentation/src/main/java/com/lanit_tercom/dogfriendly_studproject/ui/fragment/WalkFragment.java
@@ -0,0 +1,37 @@
+package com.lanit_tercom.dogfriendly_studproject.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lanit_tercom.dogfriendly_studproject.R;
+
+public class WalkFragment extends BaseFragment {
+
+
+    public WalkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_walk, container, false);
+    }
+
+    @Override
+    public void initializePresenter() {
+
+    }
+}
