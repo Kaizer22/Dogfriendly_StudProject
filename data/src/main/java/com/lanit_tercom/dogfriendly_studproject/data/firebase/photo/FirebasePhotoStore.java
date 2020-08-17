5c1d937f6b031c4056936221429c939c8ab95c9b
Nikolay <nikolaygorokhov1@gmail.com>
2020-08-17 21:13:34 +0300

    ...

--- data/src/main/java/com/lanit_tercom/dogfriendly_studproject/data/firebase/photo/FirebasePhotoStore.java
+++ data/src/main/java/com/lanit_tercom/dogfriendly_studproject/data/firebase/photo/FirebasePhotoStore.java
@@ -43,9 +43,29 @@ public class FirebasePhotoStore implements PhotoStore{
     @Override
     public void pushPhoto(String fileName, String uriString, PushPhotoCallback pushPhotoCallback) {
         StorageReference fileReference = storageReference.child(fileName);
-        fileReference.putFile(Uri.parse(uriString))
-                .addOnSuccessListener(taskSnapshot -> pushPhotoCallback.onPhotoPushed(fileReference.getDownloadUrl().toString()))
-                .addOnFailureListener(e -> pushPhotoCallback.onError(new RepositoryErrorBundle(e)));
+        UploadTask uploadTask = fileReference.putFile(Uri.parse(uriString));
+
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    pushPhotoCallback.onError(new RepositoryErrorBundle(task.getException()));
+                }
+
+                // Continue with the task to get the download URL
+                return fileReference.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    pushPhotoCallback.onPhotoPushed(downloadUri.toString());
+                } else {
+                    pushPhotoCallback.onError(new RepositoryErrorBundle(task.getException()));
+                }
+            }
+        });
 
 
     }
@@ -59,3 +79,6 @@ public class FirebasePhotoStore implements PhotoStore{
 
 
 }
+
+
+//"file:///data/user/0/com.lanit_tercom.dogfriendly_studproject/cache/cropped3621425840260292689.jpg"
